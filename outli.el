fe2ad01549dc5921e7947c5514f2750bc8c9e5c5
JD Smith <93749+jdtsmith@users.noreply.github.com>
2022-09-16 16:10:59 -0400

    Commentary

--- outli.el
+++ outli.el
@@ -25,16 +25,18 @@
 
 ;;; Commentary:
 
-;; outli-mode us a simple minor-mode overlaying outline-minor-mode
-;; which supports special comment lines as level-aware outline
-;; headings.  outli styles headings for easy level recognition and
-;; provides a org-mode-like header navigation and editing
-;; capabilities, including speed keys activated at the start of
-;; headings.
+;; outli-mode is a simple minor-mode on top of outline-minor-mode
+;; which supports configurable special comment lines as level-aware
+;; outline headings which can be collapsed, navigated, moved, etc.
+;; outli styles headings for easy level recognition and provides a
+;; org-mode-like header navigation and editing capabilities, including
+;; speed keys activated at the start of headings.
 ;; 
-;; Customize `outli-heading-config' to set the "stem" and repeated
-;; character for comment-based headings and influence how the headings
-;; are styled.
+;; Customize `outli-heading-config' to set the "stem" and "repeat"
+;; character for comment-based headings and to influence how the
+;; headings are styled.  Customize `outli-speed-commands' to alter or
+;; disable speed keys, which work at the beginning of heading lines
+;; only.
 
 ;;; Code:
 
